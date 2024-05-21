651b3ee81b4e3a70adf9cfb3c45d440616b4327e
MichaelHinrichs <mchinrichs@gmail.com>
2024-05-21 12:29:18 -0500

    Move PCK extraction to it's own file

---
+++ PCK.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infinite_Crisis_Extractor
+{
+    class PCK
+    {
+        public static void Read(string[] args)
+        {
+            br.ReadInt32();
+            br.ReadInt32();
+            br.ReadInt32();
+            br.ReadInt32();
+            br.ReadInt32();
+            br.ReadInt32();
+            br.ReadInt32();
+            br.ReadInt32();
+            br.ReadInt32();
+            br.ReadInt32();
+            br.ReadInt32();
+            br.ReadInt32();
+            int fileCount = br.ReadInt32();
+
+            List<Subfile> subfiles = [];
+
+            for (int i = 0; i < fileCount; i++)
+            {
+                subfiles.Add(new());
+            }
+            Directory.CreateDirectory(Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]));
+            int n = 0;
+            foreach (Subfile subfile in subfiles)
+            {
+                br.BaseStream.Position = subfile.offset;
+                using FileStream fs = File.Create(Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]) + "//" + n);
+                BinaryWriter bw = new(fs);
+                bw.Write(br.ReadBytes(subfile.size));
+                bw.Close();
+                fs.Close();
+                n++;
+            }
+        }
+        class Subfile
+        {
+            public int unknown1 = br.ReadInt32();
+            public int unknown2 = br.ReadInt32();
+            public int size = br.ReadInt32();
+            public long offset = br.ReadInt64();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,45 +9,6 @@ internal class Program
         if (new string(br.ReadChars(4)) != "AKPK")
             throw new System.Exception("Wrong file. Input a pck file from Infinite Crisis.");
 
-        br.ReadInt32();
-        br.ReadInt32();
-        br.ReadInt32();
-        br.ReadInt32();
-        br.ReadInt32();
-        br.ReadInt32();
-        br.ReadInt32();
-        br.ReadInt32();
-        br.ReadInt32();
-        br.ReadInt32();
-        br.ReadInt32();
-        br.ReadInt32();
-        int fileCount = br.ReadInt32();
-
-        List<Subfile> subfiles = [];
-
-        for (int i = 0; i < fileCount; i++)
-        {
-            subfiles.Add(new());
-        }
-        Directory.CreateDirectory(Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]));
-        int n = 0;
-        foreach (Subfile subfile in subfiles)
-        {
-            br.BaseStream.Position = subfile.offset;
-            using FileStream fs = File.Create(Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]) + "//" + n);
-            BinaryWriter bw = new(fs);
-            bw.Write(br.ReadBytes(subfile.size));
-            bw.Close();
-            fs.Close();
-            n++;
-        }
-    }
-
-    class Subfile
-    {
-        public int unknown1 = br.ReadInt32();
-        public int unknown2 = br.ReadInt32();
-        public int size = br.ReadInt32();
-        public long offset = br.ReadInt64();
+        Infinite_Crisis_Extractor.PCK.Read();
     }
 }
